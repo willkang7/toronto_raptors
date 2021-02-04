e5e489774935dc21c1f776a46dae2b67afa91651
William Kang <willkang7@gmail.com>
2021-02-04 00:54:27 -0500

    Added assists and rebounds statistics.

--- data.py
+++ data.py
@@ -8,28 +8,49 @@ total_points = {
 	'danny green': [],
 	'marc gasol': [],
 	}
-
+total_assists = {
+	'kawhi leonard': [],
+	'kyle lowry': [],
+	'pascal siakam': [],
+	'danny green': [],
+	'marc gasol': [],
+	}
+total_rebounds = {
+	'kawhi leonard': [],
+	'kyle lowry': [],
+	'pascal siakam': [],
+	'danny green': [],
+	'marc gasol': [],
+	}
 filenames = [
 	'magic_1.csv', 'magic_2.csv', 'magic_3.csv', 'magic_4.csv', 'magic_5.csv',
 	'76ers_1.csv', '76ers_2.csv', '76ers_3.csv', '76ers_4.csv', '76ers_5.csv', '76ers_6.csv', '76ers_7.csv',
 	'bucks_1.csv', 'bucks_2.csv', 'bucks_3.csv', 'bucks_4.csv', 'bucks_5.csv', 'bucks_6.csv',
 	'warriors_1.csv', 'warriors_2.csv', 'warriors_3.csv', 'warriors_4.csv', 'warriors_5.csv', 'warriors_6.csv']
 
+# Find data.
 for filename in filenames:
 	with open('data/' + filename) as f:
 		reader = csv.reader(f)
 		header_row = next(reader)
 		
 		for row in reader:
-			try:
-				points = int(row[19])
-			except ValueError:
-				points = 'DNP'
-			
 			player = row[0].split('\\')[0].lower()
 			if player in total_points.keys():
+				points = int(row[19])
+				assists = int(row[14])
+				rebounds = int(row[11]) + int(row[12])
 				total_points[player].append(points)
+				total_assists[player].append(assists)
+				total_rebounds[player].append(rebounds)
 
-filename = 'points.json'
+# Save data.
+filename = 'data/points.json'
 with open(filename, 'w') as f:
 	json.dump(total_points, f, indent=4)
+filename = 'data/assists.json'
+with open(filename, 'w') as f:
+	json.dump(total_assists, f, indent=4)
+filename = 'data/rebounds.json'
+with open(filename, 'w') as f:
+	json.dump(total_rebounds, f, indent=4)
--- graph.py
+++ graph.py
@@ -1,16 +1,85 @@
 import json
 import plotly.graph_objects as go
 
-filename = 'points.json'
+# Get data.
+filename = 'data/points.json'
 with open(filename) as f:
 	total_points = json.load(f)
+filename = 'data/assists.json'
+with open(filename) as f:
+	total_assists = json.load(f)
+filename = 'data/rebounds.json'
+with open(filename) as f:
+	total_rebounds = json.load(f)
 
+# Create graph.
 fig = go.Figure()
 
 for player, points in total_points.items():
-	fig.add_trace(go.Scatter(y=points, mode='lines+markers',
-		name=player.title()))
+	fig.add_trace(
+		go.Scatter(
+			y=points,
+			mode='lines+markers',
+			name=player.title(),
+		),
+	)
 
-fig.update_layout(title='Toronto Raptors Playoff Stats', hovermode="x unified",
-	xaxis_title='Game', yaxis_title='Points')
+# Add buttons for graphs of points, assists, and rebounds.
+fig.update_layout(
+	updatemenus=[
+		dict(
+			type="buttons",
+			direction="right",
+			x=0.7,
+			y=1.2,
+			showactive=True,
+			buttons=list(
+				[
+					dict(
+						label="Points",
+						method="update",
+						args=[{"y":[
+							total_points['kawhi leonard'],
+							total_points['kyle lowry'],
+							total_points['pascal siakam'],
+							total_points['danny green'],
+							total_points['marc gasol'],
+						]}],
+					),
+					dict(
+						label="Assists",
+						method="update",
+						args=[{"y":[
+							total_assists['kawhi leonard'],
+							total_assists['kyle lowry'],
+							total_assists['pascal siakam'],
+							total_assists['danny green'],
+							total_assists['marc gasol'],
+						]}],
+					),
+					dict(
+						label="Rebounds",
+						method="update",
+						args=[{"y":[
+							total_rebounds['kawhi leonard'],
+							total_rebounds['kyle lowry'],
+							total_rebounds['pascal siakam'],
+							total_rebounds['danny green'],
+							total_rebounds['marc gasol'],
+						]}],
+					),
+				]
+			),
+		)
+	]
+)
+
+# Update titles and hovermode.
+fig.update_layout(
+	title='Toronto Raptors Playoffs Stats',
+	hovermode="x unified",
+	xaxis_title='Game',
+	yaxis_title='Total',
+)
+
 fig.show()
